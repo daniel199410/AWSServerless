05647afa2f78e0939a3029968a05cd1fa7192c05
--get <--get>
2021-12-21 15:20:22 -0500

    Delete function

--- src/deleteTask.js
+++ src/deleteTask.js
@@ -1,9 +1,14 @@
 const AWS = require('aws-sdk');
 const deleteTask = async (event) => {
     const dynamoDB = new AWS.DynamoDB.DocumentClient()
-
+    const {id} = event.pathParameters;
+    await dynamoDB.delete({
+        TableName: 'Task',
+        Key: {id}
+    }).promise();
     return {
-        status: 200
+        status: 200,
+        body: {message: 'Task deleted'}
     }
 }
 module.exports = {deleteTask}
